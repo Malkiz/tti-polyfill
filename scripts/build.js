5e2218321cbcf25d144188011de931cde755d43e
omri eyal <omri.ey@walkme.com>
2019-10-15 11:45:59 +0300

    remove comments

--- scripts/build.js
+++ scripts/build.js
@@ -19,14 +19,12 @@
 /* eslint-disable no-console, require-jsdoc */
 
 
-// const chalk = require('chalk');
 const fs = require('fs-extra');
 const compiler = require('@ampproject/rollup-plugin-closure-compiler');
 const gzipSize = require('gzip-size');
 const {rollup} = require('rollup');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const path = require('path');
-// const {SourceMapGenerator, SourceMapConsumer} = require('source-map');
 
 
 const generateRollupBundle =
@@ -63,66 +61,6 @@ const generateRollupBundle =
   });
 };
 
-/* const compileRollupBundle = (outputFilePath, defines, rollupBundle) => {
-  const closureResult = compile({
-    jsCode: [{
-      src: rollupBundle.code,
-      path: path.basename(outputFilePath),
-    }],
-    defines: defines,
-    compilationLevel: 'ADVANCED',
-    useTypesForOptimization: true,
-    outputWrapper:
-        '(function(){%output%})();\n' +
-        `//# sourceMappingURL=${path.basename(outputFilePath)}.map`,
-    assumeFunctionWrapper: true,
-    rewritePolyfills: false,
-    warningLevel: 'VERBOSE',
-    createSourceMap: true,
-    externs: [{
-      src: fs.readFileSync('./src/externs.js', 'utf-8'),
-    }],
-  });
-
-  if (closureResult.errors.length || closureResult.warnings.length) {
-    const rollupMap = new SourceMapConsumer(rollupBundle.map);
-
-    // Remap errors from the closure compiler output to the original
-    // files before rollup bundled them.
-    const remap = (type) => (item) => {
-      let {line, column, source} = rollupMap.originalPositionFor({
-        line: item.lineNo,
-        column: item.charNo,
-      });
-      source = String(source); // Source can be null in some cases.
-      return {type, line, column, source, desc: item.description};
-    };
-
-    reportCompileErrorsAndExit(outputFilePath, [
-      ...closureResult.errors.map(remap('error')),
-      ...closureResult.warnings.map(remap('warning')),
-    ]);
-  } else {
-    // Currently, closure compiler doesn't support applying its generated
-    // source map to an existing source map, so we do it manually.
-    const fromMap = JSON.parse(closureResult.sourceMap);
-    const toMap = rollupBundle.map;
-
-    const generator = SourceMapGenerator.fromSourceMap(
-        new SourceMapConsumer(fromMap));
-
-    generator.applySourceMap(
-        new SourceMapConsumer(toMap), path.basename(outputFilePath));
-
-    const sourceMap = generator.toString();
-
-    return {
-      code: closureResult.compiledCode,
-      map: sourceMap,
-    };
-  }
-};*/
-
 
 const saveCompiledBundle = (outputFilePath, {code, map}) => {
   fs.outputFileSync(outputFilePath, code, 'utf-8');
@@ -130,18 +68,6 @@ const saveCompiledBundle = (outputFilePath, {code, map}) => {
   const size = (gzipSize.sync(code) / 1000).toFixed(1);
   console.log(`Built ${outputFilePath} (${size} Kb gzipped)`);
 };
-
-
-/* const reportCompileErrorsAndExit = (outputFilePath, errors) => {
-  console.error(`\nOops, there were issue compiling ${outputFilePath}\n`);
-  for (let {source, line, column, desc, type} of errors) {
-    const color = chalk[type == 'error' ? 'red' : 'yellow'];
-
-    console.error(`${color(`[${type}]`)} ${desc}`);
-    console.error(chalk.gray(`${source} [${line}:${column}]\n`));
-  }
-  process.exit(1);
-};*/
 
 
 const build = (entryFilePath, outputFilePath, externs, minify = true) => {
